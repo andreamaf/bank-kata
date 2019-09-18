4d77997f0a296ae74f8afbcecfac6a8a8214defe
Andrea Maffezzoli <andreamaf@gmail.com>
2019-09-18 14:06:44 +0200

    test captured stdout

--- src/bank.py
+++ src/bank.py
@@ -1,21 +1,25 @@
 
 class Account():
     def __init__(self, start_balance=0):
-        self.balance = start_balance
-        self.transactions_history = []
+        self._balance = start_balance
+        self._transactions_history = []
+
+    @property
+    def balance(self):
+        return self._balance
 
     def get_transactions_history(self):
         str_history = ["%s\t|| %s\t|| %s" % (date, amount, balance)
-                        for date, amount, balance in self.transactions_history]
+                        for date, amount, balance in self._transactions_history]
         return str_history[::-1]
 
-    def deposit(self, amount, date):
-        self.balance += amount
-        self.transactions_history.append((date, amount, self.balance))
+    def deposit(self, amount):
+        self._balance += amount
+        self._transactions_history.append((date, amount, self._balance))
 
-    def withdrawal(self, amount, date):
-        self.balance -= amount
-        self.transactions_history.append((date, -amount, self.balance))
+    def withdrawal(self, amount):
+        self._balance -= amount
+        self._transactions_history.append((date, -amount, self._balance))
 
     def print_statement(self):
         for transaction in self.get_transactions_history():
--- src/tests/test_account.py
+++ src/tests/test_account.py
@@ -8,7 +8,14 @@ def account():
     return Account()
 
 
-def test_acceptance_test(account):
+@pytest.fixture
+def account_start_balance():
+    return Account(start_balance=789)
+
+
+@pytest.fixture
+def acceptance_account():
+    account = Account()
 
     depo1 = (1000, '10/01/2012')
     account.deposit(*depo1)
@@ -18,11 +25,33 @@ def test_acceptance_test(account):
 
     withdr1 = (500, '14/01/2012' )
     account.withdrawal(*withdr1)
+    return account
 
+
+def test_deposit_transaction(account):
+    account.deposit(1000, '01/01/2001')
+    assert account.balance == 1000
+
+
+def test_deposit_transaction_start_balance(account_start_balance):
+    account_start_balance.deposit(1000, '01/01/2001')
+    assert account_start_balance.balance == 1789
+
+
+
+def test_transactions_history(acceptance_account):
     expected = [
                 "14/01/2012\t|| -500\t|| 2500",
                 "13/01/2012\t|| 2000\t|| 3000",
                 "10/01/2012\t|| 1000\t|| 1000"
                ]
+    assert acceptance_account.get_transactions_history() == expected
 
-    assert account.get_transactions_history() == expected
+
+def test_acceptance_test(acceptance_account, capsys):
+    acceptance_account.print_statement()
+    expected = ("14/01/2012\t|| -500\t|| 2500\n"
+                "13/01/2012\t|| 2000\t|| 3000\n"
+                "10/01/2012\t|| 1000\t|| 1000\n")
+    captured = capsys.readouterr()
+    assert captured.out == expected
